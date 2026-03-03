c1172321cb5d885ef67a73e4d5694e2c4acd7181
DESKTOP-2500GIQ\Admin <choijione02@gmail.com>
2026-03-03 09:31:35 +0900

    데이터 청크 생성 과정을 보여주는 로그 추가

--- DataPack/DataChunkGenerator/Source/Src/Main.cpp
+++ DataPack/DataChunkGenerator/Source/Src/Main.cpp
@@ -13,32 +13,43 @@
 void GenerateBallDataChunk()
 {
 	std::string dataName = "Ball";
+	std::cout << std::format("[{0}] Generating data chunk...", dataName) << std::endl;
 
 	BallDataChunk dataChunk;
 	dataChunk.DataPacks = GenerateBallDataPacks(std::format("CSV/{0}.csv", dataName));
 
-	if (DataChunkUtils::TrySaveDataChunk(std::format("Resource/{0}.bytes", dataName), dataChunk))
+	if (!DataChunkUtils::TrySaveDataChunk(std::format("Resource/{0}.bytes", dataName), dataChunk))
 	{
+		std::cout << std::format("[{0}] Error: Failed to generate data chunk!", dataName) << std::endl;
 		return;
 	}
+
+	std::cout << std::format("[{0}] Successfully generated data chunk.", dataName) << std::endl;
 }
 
 void GeneratePlayerDataChunk()
 {
 	std::string dataName = "Player";
+	std::cout << std::format("[{0}] Generating data chunk...", dataName) << std::endl;
 
 	PlayerDataChunk dataChunk;
 	dataChunk.DataPacks = GeneratePlayerDataPacks(std::format("CSV/{0}.csv", dataName));
 
-	if (DataChunkUtils::TrySaveDataChunk(std::format("Resource/{0}.bytes", dataName), dataChunk))
+	if (!DataChunkUtils::TrySaveDataChunk(std::format("Resource/{0}.bytes", dataName), dataChunk))
 	{
+		std::cout << std::format("[{0}] Error: Failed to generate data chunk!", dataName) << std::endl;
 		return;
 	}
+
+	std::cout << std::format("[{0}] Successfully generated data chunk.", dataName) << std::endl;
 }
 
 int main(int argc, char* argv[])
 {
+	std::cout << "--- Start Data Chunk Generation ---" << std::endl;
 	GenerateBallDataChunk();
 	GeneratePlayerDataChunk();
+	// NOTE: 여기 아래에 계속해서 추가.
+	std::cout << "--- Data Chunk Generation Finished ---" << std::endl;
 	return 0;
 }
